bf779b5d1ae5b72ffe2738b2ea2ac6415c9c69d9
root <root@95-163-236-9.cloudvps.regruhosting.ru>
2023-07-14 15:44:45 +0300

    Добавлена перезапись файла summary

--- book.py
+++ book.py
@@ -10,9 +10,19 @@ def get_first_level_heading(file_path):
             return match.group(1).strip()
         return None
 
-def add_heading_to_summary(heading, file_name):
+def get_second_level_headings(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+        matches = re.findall(r'^##\s+(.*)$', content, re.MULTILINE)
+        return [match.strip() for match in matches]
+
+def add_heading_to_summary(heading, file_name, indent):
     with open('SUMMARY.md', 'a', encoding='utf-8') as file:
-        file.write(f'* [{heading}](./{file_name})\n')
+        file.write(f'{" " * indent}* [{heading}](./{file_name}#{heading.lower().replace(" ", "-")})\n')
+
+# Очищаем содержимое файла SUMMARY.md
+with open('SUMMARY.md', 'w', encoding='utf-8') as file:
+    file.write('# Summary\n\n')
 
 # Получаем список всех файлов md в корневой директории
 md_files = [file for file in os.listdir() if file.endswith('.md')]
@@ -20,10 +30,18 @@ md_files = [file for file in os.listdir() if file.endswith('.md')]
 if md_files:
     # Получаем первый заголовок первого уровня из каждого файла и добавляем его в summary
     for file_name in md_files:
+        if file_name == 'SUMMARY.md':
+            continue  # Пропускаем файл SUMMARY.md
         file_path = os.path.join(os.getcwd(), file_name)
         first_level_heading = get_first_level_heading(file_path)
         if first_level_heading:
-            add_heading_to_summary(first_level_heading, file_name)
+            add_heading_to_summary(first_level_heading, file_name, 0)
+        
+        second_level_headings = get_second_level_headings(file_path)
+        if second_level_headings:
+            for heading in second_level_headings:
+                indent = 4  # Уровень отступа для заголовков второго уровня
+                add_heading_to_summary(heading, file_name, indent)
 
 # Запускаем команду npx honkit serve
 subprocess.run(['npx', 'honkit', 'serve'])
